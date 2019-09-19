b78f3815d34fed7ed462e7eb91cd2400eb2207a9
Sebastiaan Luca <hello@sebastiaanluca.com>
2019-09-19 11:42:34 +0200

    Apply code style fixes

--- src/ChangelogServiceProvider.php
+++ src/ChangelogServiceProvider.php
@@ -1,9 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SebastiaanLuca\Changelog;
 
 use Illuminate\Support\ServiceProvider;
 
 class ChangelogServiceProvider extends ServiceProvider
 {
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register() : void
+    {
+        $this->configure();
+    }
+
+    /**
+     * Register the package configuration.
+     *
+     * @return void
+     */
+    private function configure() : void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/changelog.php', 'laravel-changelog');
+    }
 }
